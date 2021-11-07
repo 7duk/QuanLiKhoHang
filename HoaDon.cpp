20f6ab71e2cf61e3c355bb1f654dd89857d5ba34
7duk <87505489+7duk@users.noreply.github.com>
2021-11-07 20:46:44 +0700

    Update HoaDon.cpp

--- HoaDon.cpp
+++ HoaDon.cpp
@@ -1,1 +1,27 @@
+#include"HoaDon.h"
+#include<iostream>
+using namespace std;
+HoaDon::HoaDon()
+{   
+    this->IdHoadon;
+    this->IdLoaihanghoa;
+    this->IdNhacungcap;
+    this->IdNhanvien;
+    this->Trangthai;
+    this->Thanhtien;
+    this->Soluong;
+    this->Ngaynhap;
+    this->Ngayxuat;
+}
+HoaDon::~HoaDon()
+{
+
+}
+int HoaDon::getSoluong(){
+    return this->Soluong;
+}
+void HoaDon::setSoluong()
+{
+    this->Soluong=Soluong;
+}
 
